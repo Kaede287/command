44692e7254f10451009c9ec0802ec3482adcdd4e
ichikawa <kka78108@gmail.com>
2023-12-26 20:29:04 +0900

    test

--- age.py
+++ age.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-age = int( input("年齢を入力してください: ") )
+try:
 
-if age >= 18:
-    print("成人です")
-else:
-    print("未成年です")
+    age = int( input("年齢を入力してください: ") )
+
+    if age >= 18:
+        print("成人です")
+    else:
+        print("未成年です")
+
+except:
+    print("入力が正しくありません.数値を入力してください")
+
