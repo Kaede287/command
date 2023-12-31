fa5e4194ab3277d9005622b3fdeb3a757cff36c7
ichikawa <kka78108@gmail.com>
2023-12-31 14:16:35 +0900

    test

--- test.bash
+++ test.bash
@@ -9,33 +9,10 @@ ng () {
 
 res=0
 
-### 成人か判定 ###
-out=$(echo "20" | ./age.py)
-[ "${out}" = "年齢を入力してください: 成人です" ] || ng ${LINENO}
-
-### 未成年か判定 ###
-out=$(echo "17" | ./age.py)
-[ "${out}" = "年齢を入力してください: 未成年です" ] || ng ${LINENO}
-
-### 未成年か判定 ###
-out=$(echo "10" | ./age.py)
-[ "${out}" = "年齢を入力してください: 未成年です" ] || ng ${LINENO}
-
-### 文字を含むとき ###
-out=$(echo "あ" | ./age.py)
-[ "$?" = 0 ] || ng ${LINENO}
-
-### 文字と数字を含む ###
-out=$(echo "18a" | ./age.py)
-[ "$?" = 0 ] || ng ${LINENO}
-
-### 記号を含む ###
-out=$(echo "." | ./age.py)
-[ "$?" = 0 ] || ng ${LINENO}
-
-### 空文字　###
-out=$(echo "" | ./age.py)
-[ "$?" = 0 ] || ng ${LINENO}
+### 正しいコマンド ###
+day=$(date '+%Y-%m-%d')
+out=$(echo "date" | ./date.py)
+[ "${out}" = "現在の日付は ${day} です" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
